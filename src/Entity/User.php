774a6cf4abd038d3aa17a0450cb49cd99eac0bad
Koumodjo Yentchabre <yaoferdinand@outloo.de>
2022-10-08 19:50:11 +0200

    handle connection process

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -107,9 +107,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setPassword(string $password): self
     {
-        dump($password);
         $this->password = $this->passwordHasher->hashPassword($this, $password);
-        dump($this->password);
         return $this;
     }
 
